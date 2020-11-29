1109cacc778a3218e9c935ff48bdb81dac1c5a3b
axp-dev <axp-dev@yandex.com>
2020-11-29 16:56:57 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"log"
+	"time"
+)
+
+func formatTime(t time.Time) time.Time {
+	return t.Round(0)
+}
+
 func main() {
-	// Place your code here
+	localTime := time.Now()
+	exactTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("current time: %s\n", formatTime(localTime))
+	fmt.Printf("exact time: %s", formatTime(exactTime))
 }
