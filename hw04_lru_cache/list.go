af3472331c894c998c1eaebd0d7cd1b3e8341787
axp-dev <axp-dev@yandex.com>
2020-12-27 18:54:56 +0500

    Implements List

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,51 +1,114 @@
 package hw04_lru_cache //nolint:golint,stylecheck
 
 type List interface {
-	Len() int                          // длина списка
-	Front() *ListItem                  // первый элемент списка
-	Back() *ListItem                   // последний элемент списка
-	PushFront(v interface{}) *ListItem // добавить значение в начало
-	PushBack(v interface{}) *ListItem  // добавить значение в конец
-	Remove(i *ListItem)                // удалить элемент
-	MoveToFront(i *ListItem)           // переместить элемент в начало
+	Len() int
+	Front() *ListItem
+	Back() *ListItem
+	PushFront(v interface{}) *ListItem
+	PushBack(v interface{}) *ListItem
+	Remove(i *ListItem)
+	MoveToFront(i *ListItem)
 }
 
 type ListItem struct {
-	Value interface{} // значение
-	Next  *ListItem   // следующий элемент
-	Prev  *ListItem   // предыдущий элемент
+	Value interface{}
+	Next  *ListItem
+	Prev  *ListItem
 }
 
 type list struct {
-	// Place your code here
+	front *ListItem
+	back  *ListItem
+	len   int
 }
 
-func (l list) Len() int {
-	return 0
+func (l *list) Len() int {
+	return l.len
 }
 
-func (l list) Front() *ListItem {
-	return nil
+func (l *list) Front() *ListItem {
+	return l.front
 }
 
-func (l list) Back() *ListItem {
-	return nil
+func (l *list) Back() *ListItem {
+	return l.back
 }
 
-func (l list) PushFront(v interface{}) *ListItem {
-	return nil
+func (l *list) PushFront(v interface{}) *ListItem {
+	item := &ListItem{
+		Value: v,
+		Next:  l.front,
+	}
+
+	if l.len > 0 {
+		l.front.Prev = item
+	} else {
+		l.back = item
+	}
+
+	l.front = item
+	l.len++
+
+	return item
 }
 
-func (l list) PushBack(v interface{}) *ListItem {
-	return nil
+func (l *list) PushBack(v interface{}) *ListItem {
+	item := &ListItem{
+		Value: v,
+		Prev:  l.back,
+	}
+
+	if l.len > 0 {
+		l.back.Next = item
+	} else {
+		l.front = item
+	}
+
+	l.back = item
+	l.len++
+
+	return item
 }
 
-func (l list) Remove(i *ListItem) {
+func (l *list) Remove(i *ListItem) {
+	var (
+		next = i.Next
+		prev = i.Prev
+	)
 
+	if next != nil {
+		next.Prev = prev
+	} else {
+		l.back = prev
+	}
+
+	if prev != nil {
+		prev.Next = next
+	} else {
+		l.front = next
+	}
+
+	l.len--
 }
 
-func (l list) MoveToFront(i *ListItem) {
+func (l *list) MoveToFront(i *ListItem) {
+	if i.Prev == nil || l.len == 1 {
+		return
+	}
 
+	i.Prev.Next = i.Next
+
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		l.back = i.Prev
+	}
+
+	i.Prev = nil
+	i.Next = l.front
+
+	l.front.Prev = i
+	l.front = i
 }
 
 func NewList() List {
